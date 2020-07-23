bd466a476a1373c964a8cef59a5d1aa9a9d59e7d
Artem <59284335+ArtemSirobaba@users.noreply.github.com>
2020-07-23 21:16:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ bot.on('photo', async (ctx) => {
   try {
     getFile(ctx);
   } catch {
-    ctx.reply('Sorry, this video is not available');
+    ctx.reply('Sorry');
   }
 });
 
@@ -18,8 +18,10 @@ const getFile = async (ctx) => {
   function timeout(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
+
   const token = '1247297939:AAG5GFqyWSfR34C9wAx6mLnlm_qIJ4gFuOQ';
-  function fileId() {
+
+  function fileId() {        // функція яка витягує найкраще по якості зображення
     const data = ctx.update.message.photo;
     const first = data[0];
     const second = data[1];
@@ -30,43 +32,45 @@ const getFile = async (ctx) => {
   }
 
   const id = fileId();
-  const response = await request({
+
+  const response = await request({ // парсить зі сторінки лінк
     method: 'GET',
     uri: `https://api.telegram.org/bot${token}/getFile?file_id=${id.file_id}`,
   });
 
   const url = `https://api.telegram.org/file/bot${token}/${response
     .split(':')[6]
-    .replace(/[""}]/g, '')}`;
+    .replace(/[""}]/g, '')}`; // лінк з якого буде скачуватися файл
 
   const options = {
     directory: './photos/',
     filename: url.split('/')[6],
   };
 
-  download(url, options, function (err) {
+  download(url, options, function (err) {  // скачується файл
     if (err) throw err;
   });
 
-  const file = url.split('/')[6];
+  const file = url.split('/')[6]; 
 
   const arr = [];
 
   arr.push(file);
 
   const data = await Promise.all(arr);
+  
   console.log(data);
 
   // await timeout(2000);
 
   // await imagesToPdf(
-  //   [`./photos/${filename}`],
+  //   [`./photos/${filename}`], тут потрібно щоб в массив потрапило декілька значень і вмісті вони скомпілювалися в один пдф файл
   //   `./pdf/${filename.split('.')[0]}.pdf`
   // );
 
   // await timeout(1000);
 
-  // ctx.replyWithDocument({ source: `./pdf/${filename.split('.')[0]}.pdf` });
+  // ctx.replyWithDocument({ source: `./pdf/${filename.split('.')[0]}.pdf` }); відправляється користувачу
 };
 
 bot.launch();
